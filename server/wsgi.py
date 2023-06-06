70e1ca74771dad82411222bfa423276134bc9a04
Abhi Verma <abhiver@Abhis-MacBook-Pro.local>
2023-06-05 23:28:34 -0700

    nltk

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import nltk
 
 # Add your project directory to the PYTHONPATH
 base = os.getcwd()
 project_home = f'{base}/src'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
+
+nltk.download('punkt')
 
 # Import the Flask app
 from src.app import app
